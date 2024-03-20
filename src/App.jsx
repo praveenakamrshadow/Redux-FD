1db033af9484fccb7212e96fbc541b7a1280351f
praveen Sahu <praveen.pkyt327867@gmail.com>
2024-03-20 18:01:59 +0530

    implemented reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
-export default function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import { useSelector } from 'react-redux';
+
+const App = () => {
+    const { users } = useSelector((state) => state.UserReducer);
+    return (
+        <div className="m-auto w-1/2 rounded-md container p-10 mt-5 bg-red-100">
+            <h1 className="text-2xl font-bold text-red-900">User List</h1>
+            <ul>
+                {users.map((user) => {
+                    return (
+                        <>
+                            <li key={user.id}>
+                                <h1>{user.name}</h1>
+                            </li>
+                        </>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+};
+
+export default App;
